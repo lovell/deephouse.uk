fb567f8482b4d2cf2b8c2ab16d69980f2a28f9ca
Lovell Fuller <github@lovell.info>
2022-09-13 13:19:20 +0100

    Remove unused filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-const toISOString = (d) => new Date(d).toISOString();
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addFilter("toISOString", toISOString);
 };
