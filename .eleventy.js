ceb9d85d998bf2023672eff297ca65d24f51b33d
Lovell Fuller <github@lovell.info>
2022-09-25 17:25:57 +0100

    Self-host CSS, use PurgeCSS to minimise size

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const pluginPurgeCss = require("eleventy-plugin-purgecss");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPlugin(pluginPurgeCss);
   eleventyConfig.addPlugin(pluginRss);
 };
